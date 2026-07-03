a82d60cb6b584ac6badde16c5dea371f70deaac4
Taehee Kim <taehee.kim@mindscale.kr>
2026-07-03 12:50:50 +0900

    chore: tanstack start init

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import { devtools } from '@tanstack/devtools-vite'
+
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+
+import viteReact from '@vitejs/plugin-react'
+import { nitro } from 'nitro/vite'
+
+const config = defineConfig({
+  resolve: { tsconfigPaths: true },
+  plugins: [
+    devtools(),
+    nitro({ rollupConfig: { external: [/^@sentry\//] } }),
+    tanstackStart(),
+    viteReact()
+  ],
+})
+
+export default config
